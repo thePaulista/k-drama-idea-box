33f634428570cec1dba295d4e06b9d65d2b4dd05
Hedy <hedywoo@gmail.com>
2016-04-15 14:02:59 -0600

    Pass edit update feature test and functionality

---
+++ spec/support/factories.rb
@@ -0,0 +1,15 @@
+#FactoryGirl.define do
+#  factory :idea do
+#    title "Titled"
+#    description "Descripted"
+#  end
+#
+#  #sequence :title do |n|
+#  #  "#{n} Title"
+#  #end
+#
+#  #sequence :description, ["A", "B", "C"].cycle do |n|
+#  #  "#{n} Description"
+#  #end
+#end
+#
